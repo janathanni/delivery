871d4dde2e964b84daf2f2ee8117ef42b9a897db
janathanni <telemain1@hotmail.com>
2025-06-18 10:59:13 +0900

    Swagger 설정 추가

---
+++ api/src/main/java/com/delivery/api/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.delivery.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.v3.core.jackson.ModelResolver;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public ModelResolver modelResolver(ObjectMapper objectMapper) {
+        return new ModelResolver(objectMapper);
+    }
+}
